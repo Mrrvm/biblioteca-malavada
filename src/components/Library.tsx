23b6da153ce32690e6cc51d6d10781b6fab6cc0b
Mrrvm <mrrvm@hotmail.com>
2026-08-03 19:16:54 +0200

    fix sign in style

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -240,7 +240,7 @@ function Library() {
             ) : (
               <button
                 onClick={() => signIn('google')}
-                className="cursor-pointer bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors"
+                className="text-xs cursor-pointer bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors"
               >
                 Sign in
               </button>
